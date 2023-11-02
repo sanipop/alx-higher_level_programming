85cd9d52b09643830cbc202640f6ce510d8ba482
sanipop <thepersian82@gmail.com>
2023-11-02 16:54:31 -0700

    redo 2-args.py

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,12 +1,18 @@
 #!/usr/bin/python3
+
 if __name__ == "__main__":
     import sys
-   lengh = len(sys.argv)
-    if lengh == 1:
-        print("{} arguments.".format(lengh - 1))
-    elif lengh == 2:
-        print("{} argument:".format(lengh - 1))
+
+    argLen = len(sys.argv)
+
+    if argLen == 1:
+        print("No arguments provided.")
+
+    elif argLen == 2:
+        print("One argument provided:")
+
     else:
-        print("{} arguments:".format(lengh - 1))
-    for i in range(1, lengh):
-        print("{}: {}".format(i, sys.argv[i]))
+        print("{} arguments provided:".format(argLen - 1))
+
+    for i in range(1, argLen):
+        print("Argument {}: {}".format(i, sys.argv[i]))
