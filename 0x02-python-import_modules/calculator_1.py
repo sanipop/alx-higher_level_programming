577050140c6742d35212481a8f5146f4599781d6
sanipop <thepersian82@gmail.com>
2023-11-02 07:49:15 -0700

    comcom

---
+++ 0x02-python-import_modules/calculator_1.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python3
+def add(a, b):
+    """My addition function
+    Args:
+        a: first integer
+        b: second integer
+    Returns:
+        The return value. a + b
+    """
+    return (a + b)
+
+
+def sub(a, b):
+    ""the first minus the second
+    Args:
+        a: integer 1
+        b: integer 2
+    Returns:
+        subtract the two numbers
+    """
+    return (a - b)
+
+
+def mul(a, b):
+    """the product of two number
+    Args:
+        a: argument 1
+        b: argument 2
+    Returns:
+        product 1 and 2
+    """
+    return (a * b)
+
+
+def div(a, b):
+    """function for devision
+    Args:
+        a: argument 1
+        b: argument 2
+    Returns:
+        division of a and b
+    """
+    return int(a / b)
