0aac3a365e69f2c5195dfa03465fdaa38a7a2eac
sanipop <thepersian82@gmail.com>
2023-11-06 02:52:30 -0800

    redo

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -1,12 +1,4 @@
 #!/usr/bin/python3
-
 def divisible_by_2(my_list=[]):
-
-   even = []
-    for ind in range(len(my_list)):
-        if my_list[ind] % 2 == 0:
-            even.append(True)
-        else:
-            even.append(False)
-
-    return (even)
+    blist = [a % 2 == 0 for a in my_list]
+    return blist
