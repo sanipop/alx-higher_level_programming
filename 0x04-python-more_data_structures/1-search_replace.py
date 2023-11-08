f0c70ea6dcc5f76bd61942403b22e34646b58dba
sanipop <thepersian82@gmail.com>
2023-11-08 14:34:06 -0800

    task 2

---
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+
+def search_replace(my_list, search, replace):
+    if my_list is None:
+        return
+    mod_list = my_list[:]
+    for cell, val in enumerate(new_list):
+        if val == search:
+            mod_list[cell] = replace
+    return mod_list
