8faed613ded7bf2cd9df3f78cd02cbf516f1e45b
sanipop <thepersian82@gmail.com>
2023-11-08 14:05:28 -0800

    Hello

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
     sq_matrix = [row[:] for row in matrix]
-    for cell, row in enumerate(new_matrix):
-        for cell2, col in enumerate(new_matrix):
+    for cell, row in enumerate(sq_matrix):
+        for cell2, col in enumerate(sq_matrix):
             sq_matrix[cell][cell2] = row[cell2] ** 2
     return sq_matrix 
