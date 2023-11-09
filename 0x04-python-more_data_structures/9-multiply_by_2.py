08d78a3955021259f1d9fa75f42d939b06b67587
sanipop <thepersian82@gmail.com>
2023-11-08 17:38:57 -0800

    mul2

--- 0x04-python-more_data_structures/9-multiply_by_2.py
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -1,6 +1,4 @@
 #!/usr/bin/python3
+
 def multiply_by_2(a_dictionary):
-    mul_dic = {}
-    for index in a_dictionary:
-        mul_dic[index] = a_dictionary[index] * 2
-    return mul_dic
+    return {key: value * 2 for key, value in a_dictionary.items()}
