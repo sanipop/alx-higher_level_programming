3f9a0bb36d3bfa2130b362770292558d599aef85
sanipop <thepersian82@gmail.com>
2023-11-08 18:02:30 -0800

    u

--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -1,13 +1,3 @@
 #!/usr/bin/python3
-
-def uniq_add(my_list=None):
-    """
-    Add unique elements of the given list and return the sum.
-
-    Parameters:
-    - my_list (list or None): The input list. Defaults to an empty list if None.
-
-    Returns:
-    - int: The sum of unique elements in the list.
-    """
-    return sum(set(my_list)) if my_list else 0
+def uniq_add(my_list=[]):
+    return(sum(set(my_list)))
