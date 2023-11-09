714fb948d8406fb7f88cf9d63f7d9f0fd0dc5da2
sanipop <thepersian82@gmail.com>
2023-11-08 18:27:05 -0800

    9

--- 0x04-python-more_data_structures/9-multiply_by_2.py
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -1,13 +1,7 @@
 #!/usr/bin/python3
 
 def multiply_by_2(a_dictionary):
-    """
-    Multiply each value in the given dictionary by 2 and return a new dictionary.
-
-    Parameters:
-    - a_dictionary (dict): The input dictionary.
-
-    Returns:
-    - dict: A new dictionary where each value is multiplied by 2.
-    """
-    return {key: value * 2 for key, value in a_dictionary.items()}
+    new_dic = {}
+    for i in a_dictionary:
+        new_dic[i] = a_dictionary[i] * 2
+    return new_dic
