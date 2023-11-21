5b56cd5ac3e1695f6f3c22d4defd4ac92b5560d0
sanipop <thepersian82@gmail.com>
2023-11-21 01:23:22 +0000

    boo

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
-#o function that printis an integer using tryby
-
+# function that printis an integer using tryby
 def safe_print_integer(value):
     try:
         print("{:d}".format(value))
