d3d33bf5232667fb172fcfb1527086a0905680c8
sanipop <thepersian82@gmail.com>
2023-11-22 01:54:18 +0000

    the first 2 Advanced

---
+++ 0x06-python-classes/100-singly_linked_list.py
@@ -0,0 +1,77 @@
+#!/usr/bin/python3
+"""Creating a class of singly linked list."""
+
+class Node
+    """Defining the class of Node."""
+
+    def __init__(self, data, next_node=None):
+        """the function of the initialisatio.
+
+        Args:
+            data (int): the variable passed of the nodee.
+            next_node (Node): node pointing to the nest list.
+        """
+        self.data = data
+        self.next_node = next_node
+
+    @property
+    def data(self):
+        """setting the data to the variable."""
+        return (self.__data)
+
+    @data.setter
+    def data(self, value):
+        if not isinstance(value, int):
+            raise TypeError("data must be an integer")
+        self.__data = value
+
+    @property
+    def next_node(self):
+        """Get/set the next_node of the Node."""
+        return (self.__next_node)
+
+    @next_node.setter
+    def next_node(self, value):
+        if not isinstance(value, Node) and value is not None:
+            raise TypeError("next_node must be a Node object")
+        self.__next_node = value
+
+
+class SinglyLinkedList:
+    """passing the data of linked listt."""
+
+    def __init__(self):
+        """the initializer of the singly."""
+        self.__head = None
+
+    def sorted_insert(self, value):
+        """Function to insert data to the listt.
+
+        passing the value at an index.
+
+        Args:
+            value (Node): the value passed.
+        """
+        new = Node(value)
+        if self.__head is None:
+            new.next_node = None
+            self.__head = new
+        elif self.__head.data > value:
+            new.next_node = self.__head
+            self.__head = new
+        else:
+            tmp = self.__head
+            while (tmp.next_node is not None and
+                    tmp.next_node.data < value):
+                tmp = tmp.next_node
+            new.next_node = tmp.next_node
+            tmp.next_node = new
+
+    def __str__(self):
+        """printing the linked list."""
+        values = []
+        tmp = self.__head
+        while tmp is not None:
+            values.append(str(tmp.data))
+            tmp = tmp.next_node
+        return ('\n'.join(values))
---
+++ 0x06-python-classes/101-square.py
@@ -0,0 +1,70 @@
+#!/usr/bin/python3
+"""Declearing the function."""
+
+
+class Square:
+    """Creating a class square."""
+
+    def __init__(self, size=0, position=(0, 0)):
+        """Initializer of the class.
+
+        Args:
+            size (int): The argument of type int.
+            position (int, int): a 2 dimension shape.
+        """
+        self.size = size
+        self.position = position
+
+    @property
+    def size(self):
+        """set the coresponding value to size."""
+        return (self.__size)
+
+    @size.setter
+    def size(self, value):
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    @property
+    def position(self):
+        """Assign the corresponding value to position."""
+        return (self.__position)
+
+    @position.setter
+    def position(self, value):
+        if (not isinstance(value, tuple) or
+                len(value) != 2 or
+                not all(isinstance(num, int) for num in value) or
+                not all(num >= 0 for num in value)):
+            raise TypeError("position must be a tuple of 2 positive integers")
+        self.__position = value
+
+    def area(self):
+        """Calculate the area of the square."""
+        return (self.__size ** 2)
+
+    def my_print(self):
+        """outputh the # square."""
+        if self.__size == 0:
+            print("")
+            return
+
+        [print("") for i in range(0, self.__position[1])]
+        for i in range(0, self.__size):
+            [print(" ", end="") for j in range(0, self.__position[0])]
+            [print("#", end="") for k in range(0, self.__size)]
+            print("")
+
+    def __str__(self):
+        """output the output of the square."""
+        if self.__size != 0:
+            [print("") for i in range(0, self.__position[1])]
+        for i in range(0, self.__size):
+            [print(" ", end="") for j in range(0, self.__position[0])]
+            [print("#", end="") for k in range(0, self.__size)]
+            if i != self.__size - 1:
+                print("")
+        return ("")
