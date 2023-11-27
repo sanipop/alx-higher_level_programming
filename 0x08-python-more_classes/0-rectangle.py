175c0299451ebf29150e205df0b518938363d2fe
sanipop <thepersian82@gmail.com>
2023-11-27 23:37:10 +0000

    test 1

---
+++ 0x08-python-more_classes/0-rectangle.py
@@ -0,0 +1,8 @@
+hebang of python 3.
+"""
+
+
+class Rectangle:
+    """A simple class that is ment to pass simply.
+    """
+    pass
---
+++ 0x08-python-more_classes/README.md
@@ -0,0 +1,1 @@
+ this is the project on 0x08. Python - More Classes and Objects with one advanced
