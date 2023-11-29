605944afac44d070d3f94add7aaeb0fbf186f275
sanipop <thepersian82@gmail.com>
2023-11-29 00:53:55 +0000

    Adv

---
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+# 101-locked_class.py
+# Abdulmalik Sani
+"""The locked script."""
+
+
+class LockedClass:
+    """
+    Creating a locked class'.
+    """
+
+    __slots__ = ["first_name"]
