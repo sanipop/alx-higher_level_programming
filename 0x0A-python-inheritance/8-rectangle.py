da5fc2027920c13af79c79443f216bd5b8f28e0c
sanipop <thepersian82@gmail.com>
2023-12-04 15:09:45 +0000

    8 rect

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -12,7 +12,7 @@ class Rectangle(BaseGeometry):
    """Recatngle inherits from baseGeometry and cal area
    """
 
-    def __init__(self, width, height):
+   def __init__(self, width, height):
         self.integer_validator("width", width)
         self.__width = width
         self.integer_validator("height", height)
