27b83e5d716e3f40e73a90be940e7d44ec0c93bb
sanipop <thepersian82@gmail.com>
2023-12-04 13:33:26 +0000

    lookup

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+=============================
+Module for making the lookup
+=============================
+"""
+
+
+def lookup(obj):
+    """A look uo object in python"""
+
+    return dir(obj)
