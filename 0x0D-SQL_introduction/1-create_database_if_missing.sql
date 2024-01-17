ff59383df1f48b087432ef1c3a3691d6fe2536bb
sanipop <thepersian82@gmail.com>
2024-01-16 16:19:54 -0800

    phase 1

---
+++ 0x0D-SQL_introduction/1-create_database_if_missing.sql
@@ -0,0 +1,2 @@
+-- Creates the database by Abdul.
+CREATE DATABASE IF NOT EXISTS `hbtn_0c_0`;
---
+++ 0x0D-SQL_introduction/4-first_table.sql
@@ -0,0 +1,2 @@
+-- Creates a table called first_table for sanipop.
+CREATE TABLE IF NOT EXISTS `first_table` (`id` INT, `name` VARCHAR(256));
