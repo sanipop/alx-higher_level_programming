3d86f64aa9a82ed61396812e4ed1f1b6c6bbcd0d
sanipop <67932273+sanipop@users.noreply.github.com>
2024-02-18 20:12:22 +0100

    Update 14-model_city_fetch_by_state.py

--- 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
+++ 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
-""" script to prints the State object with the name passed as argument from the database
+""" script to prints the State object with 
+    the name passed as argument from the database
 """
 import sys
 from model_state import Base, State
