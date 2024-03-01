03a4a5b44e8a2d4de570d863576e19dd9110dc7f
sanipop <67932273+sanipop@users.noreply.github.com>
2024-03-01 22:29:09 +0100

    Create 10-my_github.py

---
+++ 0x11-python-network_1/10-my_github.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+Python script input github credentials
+and use git Api to display
+"""
+from requests import get, auth
+import sys
+
+
+if __name__ == "__main__":
+    url = 'https://api.github.com/user'
+    user = sys.argv[1]
+    password = sys.argv[2]
+    r = get(url, auth=auth.HTTPBasicAuth(user, password))
+    print(r.json().get('id'))
