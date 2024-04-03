93ebca0748d56c37189d8e85edf38f0e919c8f77
sanipop <67932273+sanipop@users.noreply.github.com>
2024-04-03 18:50:32 +0100

    Create 2-script.js

---
+++ 0x15-javascript-web_jquery /2-script.js
@@ -0,0 +1,5 @@
+// updates the text color of the <header> element to red (#FF0000) when the user clicks on the tag D
+
+$('div#red_header').click(function () {
+    $('header').css('color', '#FF0000');
+  });
